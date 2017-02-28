36b5f36d8cd442bf0a397c29baec829e023bf754
neequole <neequole@gmail.com>
2017-02-28 23:50:19 +0800

    Add solutions last 25-Feb

---
+++ exercises/exercises01/solution02.py
@@ -0,0 +1,9 @@
+"""
+Reference: https://codepad.remoteinterview.io/FDDLRKVEYX
+Uploaded by: Stephanie Joy Rosalia
+"""
+TotalBill = input('Total Bill:')
+Payment = input('Payment Amount:')
+TotalChange = float(Payment) - float(TotalBill)
+
+print('TotalChange: {}'.format(TotalChange))
---
+++ exercises/exercises01/solution03.py
@@ -0,0 +1,9 @@
+"""
+Uploaded by: Dionne Del Castillo Mape
+"""
+totalbill = input('Your total bill:') #input total bill
+amount = input('Your payment amount:') #input amount
+totalbilll = int(totalbill) #conversion to int
+amountt = int(amount) #conversion to int
+change = totalbilll - amountt #get the change
+print('Your change is {}' .format(change)) #display
