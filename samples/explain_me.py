8000e4ce1752ff8e8230b0907a2ef4c2c8acc212
neequole <neequole@gmail.com>
2017-03-09 23:05:20 +0800

    Add sample file for discussion 02

---
+++ samples/explain_me.py
@@ -0,0 +1,13 @@
+num1 = 20000
+num2 = 5000
+num3 = 500
+num4 = 100
+num5 = 500
+num6 = 200
+
+final = num1 + num2 + (num3 - num4) - num5 - num6
+print(f'The output is: {final}')
+
+# Can you explain what this code does?
+# As a developer, what makes this code hard for you to read?
+
