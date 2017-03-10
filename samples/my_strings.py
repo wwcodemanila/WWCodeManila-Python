606b83fa7096496c6756d7e733dbfb859d0e7b06
neequole <neequole@gmail.com>
2017-03-11 00:02:33 +0800

    Add string sample

---
+++ samples/my_strings.py
@@ -0,0 +1,12 @@
+firstname = "Adam"
+lastname = "Dough"
+# We can access each character by using <your string>[<index>]
+# Each character in a string is represented by an index number
+# starting from 0
+print(firstname[0], firstname[1])
+print(lastname[0], lastname[1])
+
+# TRY TO PRINT THE LAST CHARACTER
+#
+# TRY TO PRINT AN INDEX GREATER THAN THE LENGTH BY UNCOMMENTING THE CODE BELOW
+# firstname[10]
