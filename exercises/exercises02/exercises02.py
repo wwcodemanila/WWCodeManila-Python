3027ac0996f523058f5734d2d8553924405c27f6
neequole <neequole@gmail.com>
2017-03-11 11:45:29 +0800

    Add template for exercise2

---
+++ exercises/exercises02/exercises02.py
@@ -0,0 +1,19 @@
+""" Aling Nena's Reward System
+
+    Author:
+    Description:
+"""
+
+# You can access this by: rewards[<index>]
+# Just like strings the index starts with 0
+rewards = [
+    "Coke sakto",               # 0
+    "Boy Bawang",               # 1
+    "15pcs. Yucky candy",       # 2
+    "15pcs. Pintura candy",     # 3
+    "15PHP load",               # 4
+    "3 pcs. Dove conditioner",  # 5
+    "Cottonbuds",               # 6
+    "One sheet of Biogesic",    # 7
+    "100mL Pepper/Pintura",     # 8
+]
