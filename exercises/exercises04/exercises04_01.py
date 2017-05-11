79c58f2901f8483bfd6ccc144c0a9bcabc968be6
neequole <nicole.tibay@johngaltventures.com>
2017-05-11 23:49:45 +0800

    Add resource file for conditional statement

---
+++ exercises/exercises04/exercises04_01.py
@@ -0,0 +1,18 @@
+"""
+Aling Nena stores her soft drink stock on two refrigerators.
+She stores Coke, Sprite and Royal on her Sari-sari store's refrigerator while
+RC and 7UP can be found on her house's refrigerator.
+
+Help Aling Nena to properly respond to her customer
+when buying softdrinks.
+
+The reply will depend if the soft drink brand is on the store's ref,
+on the house's ref or none. If the customer buys a soft drink brand that is:
+    1. stored on the store, she will respond 'Got it!'
+    2. stored on the house, she will respond 'Please wait for a while!'
+    3. not sold by her, she will respond 'Sorry we do not sell that. We only
+    have <input here the soft drink brands>'
+"""
+
+customer_order = input('Hi! What soft drink brand do you want?')
+
