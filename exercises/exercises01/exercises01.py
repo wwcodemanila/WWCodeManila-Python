cb64a1dc85d32355aa31c93a0aba473fdef8b254
neequole <nicole.tibay@johngaltventures.com>
2017-06-09 23:42:04 +0800

    Add challenge description to exercise01

--- exercises/exercises01/exercises01.py
+++ exercises/exercises01/exercises01.py
@@ -1,5 +1,6 @@
 """ The Challenge
-Aling Nena’s Sari-sari store wants a robot that will ask the customer
-their total bill and payment amount and tell them their change
+Author:
+Description: Aling Nena’s Sari-sari store wants a robot that will ask the
+customer their total bill and payment amount and tell them their change
 (for now, we can allow negative change).
 """
