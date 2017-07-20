7a6dcc63043d453550921c335e9baa73beb37d57
neequole <neequole@gmail.com>
2017-07-20 20:49:10 +0800

    Delete solution02.py

--- exercises/exercises01/solution02.py
+++
@@ -1,9 +0,0 @@
-"""
-Reference: https://codepad.remoteinterview.io/FDDLRKVEYX
-Uploaded by: Stephanie Joy Rosalia
-"""
-TotalBill = input('Total Bill:')
-Payment = input('Payment Amount:')
-TotalChange = float(Payment) - float(TotalBill)
-
-print('TotalChange: {}'.format(TotalChange))
