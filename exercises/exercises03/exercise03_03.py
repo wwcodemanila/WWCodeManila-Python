34007cc7afa161de00c0b261d4f4674a78d76e8a
Nicole Tibay <neequole@gmail.com>
2018-06-08 22:34:49 +0800

    Revert "Move all to docs folder"
    This reverts commit 4dc9c33d2c7d06ec0e06372983a74264b46fcad9.

---
+++ exercises/exercises03/exercise03_03.py
@@ -0,0 +1,7 @@
+''' List Exercise 3
+Write a program that accepts a series of numbers, outputs all odd numbers in a list in ascending order, 
+and all even numbers in a list in descending order.
+Sample Input: 1,2,35,45,7,8,912,23,1
+Sample Output: [1, 1, 7, 23, 35, 45] # odd
+               [912, 8, 2] # even
+'''
